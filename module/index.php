e81321cf8cffe52e1d9e3c90cebf62770b92e6b5
charlesdesenclos <75635265+charlesdesenclos@users.noreply.github.com>
2023-03-11 20:08:27 +0100

    commit 1.2.5

---
+++ module/index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Module</title>
+    <link href="style.css" rel="stylesheet" type="text/css">
+  </head>
+  <body>
+  <main>
+  <form>
+    <hgroup>
+      <h2>Module</h2>
+    </hgroup>
+    
+    <fieldset>
+      <input type="text" placeholder="Nom Equipement" required>
+      <input type="text" placeholder="Adrese" required>
+ 
+    </fieldset>
+  
+    <button>Créer un module</button>
+  </form>
+  
+  <section>
+    
+    <br>
+    <h3>Votre compte a été créé avec succès.</h3>
+  </section>
+</main>
+  </body>
+</html>
---
+++ module/script.js
@@ -0,0 +1,5 @@
+$('button').click(function(e) {
+    e.preventDefault();
+    $('main').addClass('flip');
+  });
+  
