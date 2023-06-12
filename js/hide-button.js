6628a303fbd670231c29532e8e6b370aecfeb223
Cesara02 <90842096+Cesara02@users.noreply.github.com>
2023-06-12 16:27:10 +0200

    Suppression fichier inutiles

--- js/hide-button.js
+++
@@ -1,8 +0,0 @@
-// Masquer le formulaire d'inscription par défaut
-$('#inscription-form').hide();
-
-// Afficher le formulaire d'inscription lorsque l'utilisateur clique sur le bouton "inscription"
-$('#inscription-btn').click(function() {
-    $('#inscription-form').show();
-    $('#connexion-form').hide();
-});
